a295a5e7e280c0a96fbfa34e1699f11f6c9e7540
Clement Mutz <c.mutz@whoople.fr>
2016-10-19 18:29:32 +0200

    install continu

---
+++ global.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PATH_BASH="/bin/bash"
+export PATH_CP="/bin/cp"
+export PATH_INIT_SCRIPT="INIT_SCRIPT"
+export PATH_END_SCRIPT="END_SCRIPT"
+export PATH_CONFIGURATION="CONFIGURATION"
+export PATH_LIBRARY="LIBRARY"
+export PATH_CLIENT="client"
+export LOGFILE=./log.`date +%s`.out
+export SCRIPT_TYPE=$1
+export CONFIGURATION_CUSTOM=$2
+export INSTALL_AUTO=$3
+export MAIL="infrastructure@whoople.fr"
---
+++ install.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#########################################
+# # Copyright (c) 2016, Clement Mutz <c.mutz@whoople.fr>
+# #########################################
+# # Created by Clement Mutz
+# # Contact at c.mutz@whoople.fr
+# #########################################
+# Utilisation ./mon_script.sh arg1 arg2 arg3
+# arg1 : type de serveur installe : (server ou ipbx)
+
+# changement de repertoire courant !
+cd "$(dirname "$0")"
+
+#================== Globals ==================================================
+. global.sh
+
+#================== Functions ================================================
+[ !-d $PATH_LIBRARY ] && git clone https://github.com/cmutz/fonction_perso_bash LIBRARY
+. $PATH_LIBRARY/functions.sh
+
+#================== Verification =============================================
+### you must execute root user
+[ `whoami`  != "root" ] && println error "This script need to be launched as root." && exit 1
+
+# On verifie le nombre d'arguments
+if test $# -eq 1;then
+	println "arguments valides"
+else
+	println error "\n Usage: ${0} <arg1>"
+	println error "\n arg1 type de server installe : (server ou ipbx)"
+	exit 1
+fi
+
+# On verifie la bonne saisie
+if f_checkanswer $1 server owncloud; then
+	println error "\n arg1 attendu : (server/owncloud)"
+	exit 2
+fi
+
+#### search package lsb_release ###
+if ! type -p lsb_release > /dev/null; then
+	echo "Avant toute chose, installez le programme lsb_release (paquet lsb-release sur Debian et CentOS et redhat-lsb sur RedHat)" >&2
+	exit 2
+fi
+
+### update/upgrade/install for type distribution  ###
+detectdistro
+dist_vendor=$distro
+println info "\t$distro"
+dist_name=$(lsb_release --short --codename | tr [A-Z] [a-z])
+cd "$(dirname "$0")"		# WARNING: current directory has changed!
+
+if [[ $INSTALL_AUTO = no ]]; then
+	if  ! ask_yn_question "\t*** Vous avez une '$dist_vendor - $dist_name' ***"; then
+    	read -r -p " *** Renseigner le nom du syteme (ubuntu,debian,linux_mint) *** " dist_vendor
+        read -r -p " *** Renseigner le nom de votre version (quantal,wheezy) *** " dist_name
+    fi
+fi
+
+### mode non interactif
+if [[ $INSTALL_AUTO = yes ]]; then export DEBIAN_FRONTEND=noninteractive; fi
+
+### installation depends ###
+# SENSU #
+println info "Installation du paquet sensu autossh et de nodejs"
+apt-get update -qq
+# on ajoute la clef et le dépôt pour sensu
+wget -q http://repositories.sensuapp.org/apt/pubkey.gpg -O- | apt-key add -
+echo "deb http://repositories.sensuapp.org/apt sensu main" | tee /etc/apt/sources.list.d/sensu.list
+
+# on actualise nos listings et on installe Sensu
+apt-get update
+apt-get -y install sensu autossh
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+apt-get install -y nodejs
+
+
+# pour le fun ......
+$PATH_BASH $PATH_END_SCRIPT/resume_system.sh
+
+
+
+
+
+
